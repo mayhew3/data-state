e62a79d098e6983798c2f5868b4fab523241168b
Mayhew Seavey <commander.boffo@gmail.com>
2021-03-21 20:53:22 -0700

    Bootstrap, base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppComponent } from './app.component';
 import {NgxsModule} from '@ngxs/store';
 import {NgxsReduxDevtoolsPluginModule} from '@ngxs/devtools-plugin';
 import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
+import { CreateComponent } from './components/create/create.component';
+import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
